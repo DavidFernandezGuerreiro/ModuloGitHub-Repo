77e3a53cb890daa5764d85e55440e35b2c1ca43d
David <David@192.168.1.7>
2018-04-21 15:52:59 +0100

    Crear repositorio desde NetBeans Modules.

---
+++ src/com/david/modulogithub/IconGitActionListener.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.david.modulogithub;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
+import org.kohsuke.github.GitHub;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "File",
+        id = "com.david.modulogithub.IconGitActionListener"
+)
+@ActionRegistration(
+        iconBase = "iconGitHub.png",
+        displayName = "#CTL_IconGitActionListener"
+)
+@ActionReference(path = "Toolbars/File", position = 0)
+@Messages("CTL_IconGitActionListener=Git")
+public final class IconGitActionListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        /**
+         * @param nombreRepo Pide un nombre para el repositorio, a través de un JOptionPane.
+         * 
+         * A continuación se creará un repositorio en GitHub.
+         */
+        
+        String nombreRepo=JOptionPane.showInputDialog("Insetar nombre del repositorio: ");
+        try {
+            GitHub obxGitHub=GitHub.connect();
+            GHCreateRepositoryBuilder obxBuilder=obxGitHub.createRepository(nombreRepo);
+            obxBuilder.create();
+        } catch (IOException ex) {
+            System.out.println("ERROR: "+ex);
+        }
+        
+    }
+}
